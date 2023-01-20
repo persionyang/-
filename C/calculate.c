fe383d74a9a6193d19469cb3e4afb4ff1f6b8f27
bluen <65480922+persionyang@users.noreply.github.com>
2023-01-20 18:04:08 +0800

    c

--- C/calculate.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("23+43=%d\n", 23+43);
-
-    return 0;
-}
--- C/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("ÖĞÎÄ²âÊÔ\n");
-
-    return 0;
-}
