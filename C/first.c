e5ec4053dfba24c9f9005c6623cb325e9471e765
bluen <65480922+persionyang@users.noreply.github.com>
2023-02-02 11:00:43 +0800

    Add files via upload

---
+++ C/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int num;
+    num = 1;
+    printf("I have a simple computer.\n");
+    printf("My favorite number is %d becaude it is first.\n",num);
+    
+    getchar();
+    return 0; 
+}
