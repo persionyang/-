1372797500d9965e676e3593d8f21ad6a4e2ef25
bluen <65480922+persionyang@users.noreply.github.com>
2023-02-08 11:15:54 +0800

    Add files via upload

---
+++ C/two_func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void butler(void);
+int main(void)
+{
+    printf("我要召唤管家功能。\n");
+    butler();
+    printf("是的，给我带点茶和可写的DVD。\n");
+    
+    getchar();
+    return 0;    
+}
+void butler(void)
+{
+    printf("您要点什么吗，先生?\n");
+    
+}
